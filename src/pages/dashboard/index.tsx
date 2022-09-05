f1d20925f3ec4ba21af75de33712e8d25313eb87
PauloMtz <pramosmtz@gmail.com>
2022-09-05 06:45:27 -0300

    Página de Dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,18 +1,37 @@
 import Head from "../../../node_modules/next/head";
 import { Header } from "../../components/Header/index";
 import { usuariosLogados } from "../../utils/usuariosLogados"
+import styles from './styles.module.scss';
+
+import { FiRefreshCcw } from 'react-icons/fi';
 
 export default function Dashboard() {
     return (
-        <div>
+        <>
             <Head>
                 <title>Painel - Sujeito Pizza</title>
             </Head>
-            <div>
-                <Header />
-                <h1>Painel</h1>
-            </div>
-        </div>
+
+            <Header />
+
+            <main className={styles.container}>
+                <div className={styles.containerHeader}>
+                    <h1>Últimos pedidos</h1>
+                    <button>
+                        <FiRefreshCcw size={25} color="#3fffa3"/>
+                    </button>
+                </div>
+
+                <article className={styles.listOreders}>
+                    <section className={styles.orderItem}> 
+                        <button>
+                            <div className={styles.tag}></div>
+                            <span>Mesa 30</span>
+                        </button>
+                    </section>                  
+                </article>
+            </main>
+        </>
     )
 }
 
